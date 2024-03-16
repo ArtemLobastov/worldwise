0b8a3bc0cc1d2d4cb778a6081aa41e623d4bd852
ArtemLobastov <artemlobastov92@gmail.com>
2024-03-16 10:03:55 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Login from './pages/Login';
+
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+
+function App() {
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
