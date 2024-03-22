57b89a0b797ed76774a23d3d3ad1c171c8e5a585
ArtemLobastov <artemlobastov92@gmail.com>
2024-03-22 11:20:31 +0100

    Fix: auth

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { createContext, useContext, useReducer } from 'react';
 
 const AuthContext = createContext();
@@ -7,7 +8,7 @@ const initialState = {
 };
 function reducer(state, action) {
   switch (action.type) {
-    case 'ligon':
+    case 'login':
       return { ...state, user: action.payload, isAuthenticated: true };
     case 'logout':
       return { ...state, user: null, isAuthenticated: false };
@@ -15,16 +16,28 @@ function reducer(state, action) {
       return new Error('Invalid action type');
   }
 }
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
-  function login(email, password) {}
-  function logout() {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: 'login', payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: 'logout' });
+  }
 
   return (
-    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -36,3 +49,5 @@ function useAuth() {
     throw new Error('AuthContext was used outside AuthProvider');
   return context;
 }
+
+export { AuthProvider, useAuth };
